1c68abe8e3eadf3ea67bd24ab4dd59a140f1bea8
Vince Broz <vince@broz.cc>
2015-05-06 15:02:24 -0400

    Slamhound cleanup

--- section-4-1-2/src/section_4_1_2/and.clj
+++ section-4-1-2/src/section_4_1_2/and.clj
@@ -1,6 +1,5 @@
 (ns section-4-1-2.and
-  (:require [section-4-1-2.util :as util]
-            [section-4-1-2.boolean :as boolean]))
+  (:require [section-4-1-2.util :as util]))
 
 (defn and? [exp]
   (util/tagged-list? exp 'and))
