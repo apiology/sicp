5048a3d6a5ec9819844d525c19bcf19fc1a77598
Vince Broz <vince@broz.cc>
2015-05-06 14:13:19 -0400

    Fix reference to boolean?

--- section-4-1-2/src/section_4_1_2/primitive.clj
+++ section-4-1-2/src/section_4_1_2/primitive.clj
@@ -1,13 +1,13 @@
 (ns section-4-1-2.primitive
-  (:refer-clojure :only [defn or number? cond string? boolean?])
+  (:refer-clojure :only [defn or number? cond string?])
   (:require [section-4-1-2.util :as util]
-            [section-4-1-2.util :as boolean]))
+            [section-4-1-2.boolean :as boolean]))
 
 (defn self-evaluating? [exp]
   (cond
     (number? exp) true
     (string? exp) true
-    (boolean? exp) true
+    (boolean/boolean? exp) true
     :else false))
 
 
