71bfbf61b1944698ffcac0e9ad16c99e749e3191
Vince Broz <vince@broz.cc>
2015-05-07 11:28:32 -0400

    Fix synta error

--- section-4-1-2/project.clj
+++ section-4-1-2/project.clj
@@ -3,13 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [slamhound "1.5.5"]]
   :plugins [[lein-cloverage "1.0.2"]
             [lein-kibit "0.1.4-SNAPSHOT"]
             [lein-ancient "0.6.7"]
             [jonase/eastwood "0.2.1"]]
-  :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-  :dependencies [[slamhound "1.5.5"]])
+  :aliases {"slamhound" ["run" "-m" "slam.hound"]})
+
 
 
 
