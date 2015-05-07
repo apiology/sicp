c0119368e74e9f7f3e0b60de552bf55f85af746c
Vince Broz <vince@broz.cc>
2015-05-07 11:24:23 -0400

    Make build dependencies explicit

--- section-4-1-2/project.clj
+++ section-4-1-2/project.clj
@@ -3,6 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :plugins [[lein-cloverage "1.0.2"]
+            [lein-kibit "0.1.4-SNAPSHOT"]
+            [lein-ancient "0.6.7"]
+            [jonase/eastwood "0.2.1"]]
+  :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+  :dependencies [[slamhound "1.5.5"]])
+
 
 
