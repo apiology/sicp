2e535b12d77ece5abdae8e454536d217e8019f8b
Vince Broz <vince@broz.cc>
2015-05-08 17:05:35 -0400

    Start down the road of namedlet

--- section-4-1-2/src/section_4_1_2/let.clj
+++ section-4-1-2/src/section_4_1_2/let.clj
@@ -2,7 +2,7 @@
   (:require [section-4-1-2.lambda :as lambda]
             [section-4-1-2.util :as util])
   (:refer-clojure :only
-                  [cons defn empty? first let list map nth println rest
+                  [= < and cons count defn empty? first let list map nth println rest
                         second]))
 
 (defn let? [exp]
@@ -23,13 +23,37 @@
 (defn clauses->lambda-parameters [clauses]
   (map clause->variable clauses))
 
-(defn let->combination [exp]
-  (let [clauses (let-clauses exp)
-        bound-expression (let-bound-expression exp)]
-    (cons (lambda/make-lambda (clauses->lambda-parameters clauses)
-                              bound-expression)
-          (map clause->value clauses))))
+(defn named-let-name [exp]
+  (second exp))
 
+(defn named-let-clauses [exp]
+  (nth exp 2))
+
+(defn named-let-bound-expression [exp]
+  (nth exp 3))
+
+(defn named-let->combination [exp]
+  (let [name (named-let-name exp)
+        clauses (named-let-clauses exp)
+        bound-expression (named-let-bound-expression exp)]
+    (cons (list 'let
+                (list (list name
+                            (lambda/make-lambda (clauses->lambda-parameters clauses)
+                                                bound-expression)))
+                (cons name (map clause->value clauses))))))
+
+(defn named-let? [exp]
+  <(and (let? exp) (= (count exp) 4)))
+
+(defn let->combination [exp]
+  (if (named-let? exp)
+    (named-let->combination exp)
+    (let [clauses (let-clauses exp)
+          bound-expression (let-bound-expression exp)]
+      (cons (lambda/make-lambda (clauses->lambda-parameters clauses)
+                                bound-expression)
+            (map clause->value clauses)))))
+  
 (defn eval-let [exp env eval-fn apply-fn]
   (eval-fn (let->combination exp) env))
 
--- section-4-1-2/test/section_4_1_2/namedlet_test.clj
+++ section-4-1-2/test/section_4_1_2/namedlet_test.clj
@@ -1,14 +1,18 @@
 (ns section-4-1-2.namedlet-test
   (:require [section-4-1-2.let :refer :all]
+            [section-4-1-2.test-env :refer [test-env]]
+            [section-4-1-2.core :refer :all]
             [clojure.test :refer :all]))
 
 ;; XXX get this working
-;; (deftest namedlet-advanced
-;;   (testing ""
-;;     (is (= (eval '(define (fib n)
-;;                     (let fib-iter ((a 1)
-;;                                    (b 0)
-;;                                    (count n))
-;;                          (if (= count 0)
-;;                            b
-;;                            (fib-iter (+ a b) a (- count 1)))))
+(deftest namedlet-advanced
+   (testing ""
+     (is (= (eval '(define (fib n)
+                     (let fib-iter ((a 1)
+                                    (b 0)
+                                    (count n))
+                          (if (= count 0)
+                            b
+                            (fib-iter (+ a b) a (- count 1)))))
+                  (test-env))
+            0))))
