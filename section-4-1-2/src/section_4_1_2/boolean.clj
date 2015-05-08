8d1173fb59eda771918173d54529c5335a7958c5
Vince Broz <vince@broz.cc>
2015-05-08 12:35:34 -0400

    Adopt SICP concept of falsity

--- section-4-1-2/src/section_4_1_2/boolean.clj
+++ section-4-1-2/src/section_4_1_2/boolean.clj
@@ -1,6 +1,6 @@
 (ns section-4-1-2.boolean
   (:require [section-4-1-2.util :as util])
-  (:refer-clojure :only [defn number? or]))
+  (:refer-clojure :only [defn number? not or]))
 
 ;; false not bound in global environment yet--waiting patiently for
 ;; section 4.1.4, but in the meantime, this helps test the rest
@@ -9,10 +9,4 @@
       (clojure.core/false? exp)))
 
 (defn true? [cond]
-  (if (number? cond)
-    (clojure.core/pos? cond)
-    (if (clojure.core/true? cond)
-      true
-      (if (clojure.core/false? cond)
-        false
-        (util/error "true? not implemented on " cond)))))
+  (not (clojure.core/false? cond)))
--- section-4-1-2/test/section_4_1_2/or_test.clj
+++ section-4-1-2/test/section_4_1_2/or_test.clj
@@ -11,7 +11,7 @@
 
 (deftest or-with-one-false-arg
   (testing ""
-    (is (= (eval '(or 0) default-env) false))))
+    (is (= (eval '(or false) default-env) false))))
 
 (deftest or-with-one-true-arg
   (testing ""
@@ -19,12 +19,12 @@
 
 (deftest or-with-one-false-and-one-true-arg
   (testing ""
-    (is (= (eval '(or 0 1) default-env) 1))))
+    (is (= (eval '(or false 1) default-env) 1))))
 
 (deftest or-with-many-false-and-one-true-arg
   (testing ""
-    (is (= (eval '(or 0 false 0 12) default-env) 12))))
+    (is (= (eval '(or false false false 12) default-env) 12))))
 
 (deftest or-short-circuits
   (testing ""
-    (is (= (eval '(or 0 false 0 12 unknown-symbol) default-env) 12))))
+    (is (= (eval '(or false false false 12 unknown-symbol) default-env) 12))))
