59f5d5aacaa14334ec2a226dc0857f630aaff58c
Vince Broz <vince@broz.cc>
2015-05-08 15:24:42 -0400

    Better env handling

--- section-4-1-2/test/section_4_1_2/definition_test.clj
+++ section-4-1-2/test/section_4_1_2/definition_test.clj
@@ -1,16 +1,17 @@
 (ns section-4-1-2.definition-test
   (:require [clojure.test :refer :all]
             [section-4-1-2.core :refer :all])
-  (:refer-clojure :only [=]))
+  (:refer-clojure :only [= defn]))
 
-(def default-env (section-4-1-2.environment/extend-environment
-                  '()
-                  '()
-                  section-4-1-2.environment/the-empty-environment))
+(defn default-env []
+  (section-4-1-2.environment/extend-environment
+   '()
+   '()
+   section-4-1-2.environment/the-empty-environment))
 
 (deftest definition
   (testing ""
-    (is (= (eval '(define a 1) default-env)
+    (is (= (eval '(define a 1) (default-env))
            :ok))))
 
 (deftest definition_and_use
@@ -18,5 +19,7 @@
     (is (= (eval '(begin
                    (define a 1)
                    a)
-                 default-env)
+                 (default-env))
            1))))
+
+;; XXX test redefinition
