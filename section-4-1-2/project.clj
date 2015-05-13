dc8e5ef2d68953ff323f2ca8c84289ec7758eaf4
Vince Broz <vince@broz.cc>
2015-05-13 14:57:12 -0400

    Add eastwood plugin

--- section-4-1-2/project.clj
+++ section-4-1-2/project.clj
@@ -9,7 +9,8 @@
   :plugins [[lein-typed "0.3.5"]
             [lein-cloverage "1.0.2"]
             [lein-kibit "0.1.2"]
-            [lein-ancient "0.6.7"]]
+            [lein-ancient "0.6.7"]
+            [jonase/eastwood "0.2.1"]]
   :core.typed {:check [section-4-1-2.environment section-4-1-2.util]}
   :aliases {"slamhound" ["run" "-m" "slam.hound"]})
 
