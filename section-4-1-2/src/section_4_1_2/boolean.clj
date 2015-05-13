9c923ef56aa2593a93da27dbd81ffd225f4b1097
Vince Broz <vince@broz.cc>
2015-05-13 15:41:42 -0400

    Global environment has arrived

--- section-4-1-2/src/section_4_1_2/boolean.clj
+++ section-4-1-2/src/section_4_1_2/boolean.clj
@@ -1,8 +1,6 @@
 (ns section-4-1-2.boolean
   (:refer-clojure :only [defn not number? or]))
 
-;; false not bound in global environment yet--waiting patiently for
-;; section 4.1.4, but in the meantime, this helps test the rest
 (defn boolean? [exp]
   (or (clojure.core/true? exp)
       (clojure.core/false? exp)))
