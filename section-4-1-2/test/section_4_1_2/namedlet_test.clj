5594d653f606db3621ddf56ada27815c5b32d0a3
Vince Broz <vince@broz.cc>
2015-05-13 14:48:27 -0400

    Fix up tests

--- section-4-1-2/test/section_4_1_2/namedlet_test.clj
+++ section-4-1-2/test/section_4_1_2/namedlet_test.clj
@@ -1,18 +1,50 @@
 (ns section-4-1-2.namedlet-test
-  (:require [section-4-1-2.let :refer :all]
+  (:require [section-4-1-2.environment :as environment]
+            [section-4-1-2.let :as let]
             [section-4-1-2.test-env :refer [test-env]]
-            [section-4-1-2.core :refer :all]
-            [clojure.test :refer :all]))
+            [section-4-1-2.core :refer [eval]]
+            [clojure.test :refer :all])
+  (:refer-clojure :only [= first let second]))
+
+
+(deftest namedlet-translation
+   (testing ""
+     (is (= (let/named-let->combination '(let fib-iter ((a 1)
+                                                        (b 0)
+                                                        (count n))
+                                              (if (= count 0)
+                                                b
+                                                (fib-iter (+ a b) a (- count 1)))))
+            '(let ((fib-iter (lambda (a b count) (if (= count 0) b (fib-iter (+ a b) a (- count 1)))))) (fib-iter 1 0 n))
+            ))))
+
+(deftest namedlet-advanced
+  (testing ""
+    
+    (is (= (eval '(define (fib n)
+                    (let fib-iter ((a 1)
+                                   (b 0)
+                                   (count n))
+                         (if (= count 0)
+                           b
+                           (fib-iter (+ a b) a (- count 1)))))
+                 (test-env))
+           :ok))))
+
+;; (environment/first-frame (test-env))
 
 ;; XXX get this working
-(deftest namedlet-advanced
-   (testing ""
-     (is (= (eval '(define (fib n)
-                     (let fib-iter ((a 1)
-                                    (b 0)
-                                    (count n))
-                          (if (= count 0)
-                            b
-                            (fib-iter (+ a b) a (- count 1)))))
-                  (test-env))
-            0))))
+;; (deftest namedlet-advanced-with-use
+;;   (testing ""
+;;     (is (= (eval '(begin
+;;                    (define (fib n)
+;;                      (let fib-iter ((a 1)
+;;                                     (b 0)
+;;                                     (count n))
+;;                           (if (= count 0)
+;;                             b
+;;                             (fib-iter (+ a b) a (- count 1)))))
+;;                    (fib 2)
+;;                    1)
+;;                  (test-env))
+;;            :ok))))
