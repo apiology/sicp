4d712105f2db53e603d4eb334473ee2af8d4afaa
Vince Broz <vince@broz.cc>
2015-05-13 14:50:25 -0400

    Add lein-typed plugin

--- section-4-1-2/project.clj
+++ section-4-1-2/project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.typed "0.2.84"]
                  [slamhound "1.5.5"]]
-  :plugins [[lein-cloverage "1.0.2"]
+  :plugins [[lein-typed "0.3.5"]
+            [lein-cloverage "1.0.2"]
             [lein-kibit "0.1.2"]
             [lein-ancient "0.6.7"]]
   :core.typed {:check [section-4-1-2.environment section-4-1-2.util]}
