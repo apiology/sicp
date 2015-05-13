3f1031a94a39df71289aab1443b6ea8dbaddc49c
Vince Broz <vince@broz.cc>
2015-05-13 14:05:08 -0400

    Add missing file

---
+++ section-4-1-2/src/section_4_1_2/types.clj
@@ -0,0 +1,33 @@
+(ns section-4-1-2.types
+  (:require [clojure.core.typed :as t]))
+
+(t/defalias Var t/Symbol)
+
+(t/defalias Variables (t/Seqable Var))
+
+(t/defalias RawVal t/Any)
+
+(t/defalias RawValues (t/Seqable RawVal))
+
+(t/defalias Values (t/Seqable (t/Atom1 RawVal)))
+
+(t/defalias Frame (t/Atom1 (t/HVec [Variables Values])))
+
+(t/defalias EmptyEnvironment nil)
+
+(t/defalias Environment (t/Option (t/Seqable Frame)))
+
+(t/defalias Primitive (t/U t/Symbol Number String))
+
+(t/defalias Combination (t/List Expression))
+
+(t/defalias Expression (t/U Primitive Combination))
+
+(t/defalias Expressions (t/Seqable Expression))
+
+(t/defalias Sequence  (t/U Expressions Primitive))
+
+(t/defalias EvalFn [Expression Environment -> Expression])
+
+(t/defalias ApplyFn [Expression Expressions -> Expression])
+
