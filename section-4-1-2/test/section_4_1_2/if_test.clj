8a1a774fce9260d59b253673ab2e4e4e89e93e92
Vince Broz <vince@broz.cc>
2015-05-13 16:05:15 -0400

    Adjust to new boolean behavior

--- section-4-1-2/test/section_4_1_2/if_test.clj
+++ section-4-1-2/test/section_4_1_2/if_test.clj
@@ -1,18 +1,17 @@
 (ns section-4-1-2.if-test
   (:require [clojure.test :refer :all]
-            [section-4-1-2.core :refer :all])
+            [section-4-1-2.core :refer :all]
+            [section-4-1-2.test-env :refer [test-env]])
   (:refer-clojure :only [=]))
-
-(def default-env nil)
 
 (deftest if-false
   (testing "if-false"
-    (is (= (eval '(if false 123 456) default-env) 456))))
+    (is (= (eval '(if 'false 123 456) (test-env)) 456))))
 
 (deftest if-true
   (testing ""
-    (is (= (eval '(if 1 123 456) default-env) 123))))
+    (is (= (eval '(if 1 123 456) (test-env)) 123))))
 
 (deftest if-true-without-second
   (testing ""
-    (is (= (eval '(if 1 123) default-env) 123))))
+    (is (= (eval '(if 1 123) (test-env)) 123))))
