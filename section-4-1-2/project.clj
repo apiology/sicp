2735824c2f96ffdfa4f3917ceffde9c3b7fa0293
Vince Broz <vince@broz.cc>
2015-05-18 16:50:37 -0400

    Don't forget to check .binding

--- section-4-1-2/project.clj
+++ section-4-1-2/project.clj
@@ -11,7 +11,7 @@
             [lein-kibit "0.1.2"]
             [lein-ancient "0.6.7"]
             [jonase/eastwood "0.2.1"]]
-  :core.typed {:check [section-4-1-2.environment section-4-1-2.util]}
+  :core.typed {:check [section-4-1-2.binding section-4-1-2.environment section-4-1-2.util]}
   :aliases {"slamhound" ["run" "-m" "slam.hound"]})
 
 
