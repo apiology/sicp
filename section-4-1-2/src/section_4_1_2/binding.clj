a08ddf47702c92d427d0317059d3a2790bd6ca39
Vince Broz <vince@broz.cc>
2015-05-18 16:56:40 -0400

    Refactor

--- section-4-1-2/src/section_4_1_2/binding.clj
+++ section-4-1-2/src/section_4_1_2/binding.clj
@@ -8,10 +8,10 @@
 (def ^{:preserve [types/Expression]} from-slamhound true)
 
 ;; Use t/defn instead
-(t/ann scan-for-variable [types/Var types/Environment [types/Value -> types/RawVal]-> types/RawVal])
+(t/ann scan-for-variable (t/All [a] [types/Var types/Environment [types/Value -> a]-> a]))
 
 (defn scan-for-variable [var env f]
-  (t/letfn> [scan :- [types/Variables types/Values -> types/RawVal]
+  (t/letfn> [scan :- [types/Variables types/Values -> a]
              (scan [vars vals]
                    ;; (println "Scanning vars=" vars ", vals=" vals)
                    (cond (empty? vars)
@@ -38,37 +38,19 @@
 
 (defn lookup-variable-value [var :- types/Var
                              env :- types/Environment
-                             eval-fn :- types/EvalFn
+                             eval-fn :- types/EvalFn ;; XXX why do we get these last two params?
                              apply-fn :- types/ApplyFn] :- types/RawVal
   ;; (println "Lookup-variable-value ")
   (scan-for-variable var env value->rawval))
 
+(defn reset-value-and-return-ok [value :- types/Value] :- (t/Value :ok)
+  (reset! value val)
+  :ok)
+
 (defn set-variable-value! [var :- types/Var
                            val :- types/RawVal
                            env :- types/Environment]
-  (t/letfn> [env-loop :- [types/Environment -> (t/Value :ok)]
-             (env-loop [env]
-            (t/letfn> [scan :- [types/Variables types/Values -> (t/Value :ok)]
-                       (scan [vars vals]
-                      (cond (empty? vars)
-                            (env-loop (environment/enclosing-environment
-                                       env))
-
-                            (= var (first vars))
-                            (let [first-val (first vals)
-                                  _ (assert (not (nil? first-val)))]
-                              (reset! first-val val)
-                              :ok)
-                            
-                            :else
-                            (recur (rest vars) (rest vals))))]
-              (if (= env environment/the-empty-environment)
-                (util/error "Unbound variable -- SET!" var)
-                (let [frame (environment/first-frame env)
-                      _ (assert (not (nil? frame)))]
-                  (scan (environment/frame-variables frame)
-                        (environment/frame-values frame))))))]
-        (env-loop env)))
+  (scan-for-variable var env reset-value-and-return-ok))
 
 (defn define-variable! [var :- types/Var
                         val :- types/RawVal
